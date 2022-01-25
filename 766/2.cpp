aa7ab34e387f8a1aab49f7deb36ead5c0d4b7751
Yuno <prcups@v-signal.xyz>
2022-01-25 23:53:50 +0800

    766p

--- 766/2.cpp
+++ 766/2.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <cstdio>
-#include <cstring>
-#include <cstdlib>
+#include <algorithm>
 
 using namespace std;
 
-int T,
+int T, a, b, len;
+int c[100005];
 
 int read() {
 	int s = 0, w = 1;
@@ -14,4 +14,23 @@ int read() {
 	if (ch == '-') w = -w, ch = getchar();
 	while (ch >= '0' && ch <= '9') s = s * 10 + ch - '0', ch = getchar();
 	return s * w;
+}
+
+int main() {
+	T = read();
+	while (T--) {
+		a = read();
+		b = read();
+		len = 0;
+		for (int i = 1; i <= a; ++i) {
+			for (int j = 1; j <= b; ++j) {
+				c[++len] = max(i - 1, a - i) + max(j - 1, b - j);
+			}
+		}
+		sort(c + 1, c + len + 1);
+		for (int i = 1; i <= len; ++i) {
+			printf("%d ", c[i]);
+		}
+		printf("\n");
+	}
 }
